261eeeccc13186b809b2276b6a54c6041b191f80
tanhd <tanhd@rikkeisoft.com>
2018-04-05 17:31:37 +0700

    refactor permission

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -18,38 +18,26 @@ class RolesTableSeeder extends Seeder
                 'name' => 'Root',
                 'created_at' => Carbon::now(),
                 'slug' => 'root',
-                'permissions' =>  json_encode( array(
-                    "create-post" => true,
-                    "update-post" => true,
-                )),
+
             ],
             [
                 'id' => '2',
                 'name' => 'Secrectory',
                 'created_at' => Carbon::now(),
                 'slug' => 'secrectory',
-                'permissions' => json_encode(  array(
-                    "publish-post" => true,
-                )),
+
             ],
             [
                 'id' => '3',
                 'name' => 'Editor',
                 'created_at' => Carbon::now(),
                 'slug' => 'editor',
-                'permissions' => json_encode( array(
-                    "update-post" => true,
-                    "confirm-post" => true,
-                )),
+
             ],
             [
                 'id' => '4',
                 'name' => 'Author',
                 'slug' => 'author',
-                'permissions' =>  json_encode( array(
-                    "update-post" => true,
-                    "create-post" => true,
-                )),
                 'created_at' => Carbon::now(),
             ],
         ];
